0da88b497e59bd99687540fdbee10c827a01a2df
navee-hans <naveehans@gmail.com>
2024-09-29 16:35:46 +0530

    updated test runner file

--- src/test/java/Hooks/TestRunner.java
+++ src/test/java/Hooks/TestRunner.java
@@ -4,13 +4,18 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-import java.text.Format;
+import static Hooks.TestRunner.getFeaturePath;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = System.getProperty("features.path", "src/test/java/Features"),
-        glue = {"stepDefinition","Hooks",},
+        features = "src/test/java/Feature",
+        glue = {"stepDefinition","Hooks"},
         plugin = {"pretty", "html:target/cucumber-html-report", "json:target/cucumber-json-report.json"}
 )
 public class TestRunner {
+
+    public static String getFeaturePath() {
+        // Logic to determine feature path dynamically
+        return "src/test/java/features";
+    }
 }
